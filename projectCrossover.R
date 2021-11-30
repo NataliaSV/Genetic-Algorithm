b8ecdf9aaf9f88d7a16a6f9a432db344a846bb13
jqling53 <89624295+jqling53@users.noreply.github.com>
2021-11-29 22:26:52 -0800

    added Crossover and Mutation

---
+++ projectCrossover.R
@@ -0,0 +1,32 @@
+### STAT243 Group Project
+### Crossover
+### Jonathan Ling
+
+crossover <- function(parentsA, parentsB) {
+  
+  # Create vector of where to split each chromosome
+  # Random selection
+  splitLocation <- sample(1:(ncol(parentsA) - 1),
+                          size = nrow(parentsA), replace = T)
+  
+  # Combine genes left of the split location from parent A with
+  #   genes right of the split location from parent B
+  breedSingle <- function(A, B, splitIndex) {
+    child <- c(A[1:splitIndex], B[(splitIndex + 1):length(B)])
+    return(child)
+  }
+  
+  # Apply breedSingle to every pair of parents
+  childMatrix <- matrix(rep(0, nrow(parentsA) * ncol(parentsA)),
+                        nrow(parentsA), ncol(parentsA))
+  for (i in 1:nrow(parentsA)) {
+    childMatrix[i,] <- breedSingle(parentsA[i,], parentsB[i,], splitLocation[i])
+  }
+  return(childMatrix)
+}
+
+### Test case
+# testMatA <- matrix(sample(c(0,1), 100, replace = T),10,10)
+# testMatB <- matrix(sample(c(0,1), 100, replace = T),10,10)
+# 
+# testChild <- crossover(testMatA, testMatB)
---
+++ projectMutation.R
@@ -0,0 +1,27 @@
+### STAT243 Group Project
+### Mutation
+### Jonathan Ling
+
+
+mutate <- function(chromosomes, mutateProbability = 0.01) {
+  # Inputs: 
+  #   chromosomes: matrix of chromosomes
+  #   mutateProbability: the probability for each gene to mutate. Default
+  #                       is 1%.
+  # Output:
+  #   Matrix of mutated chromosomes
+  
+  # Mutate by subtracting 1 from the element and taking the absolute value
+  #   Changes 0 to 1, changes 1 to 0
+  # Generate mutation matrix from Bernoulli(mutateProbablity) distribution
+  mutatedChromosomes <- abs(chromosomes - 
+                            matrix(rbinom(nrow(chromosomes) * ncol(chromosomes),
+                                            1, mutateProbability), 
+                            nrow = nrow(chromosomes), ncol = ncol(chromosomes))
+                            )
+  return(mutatedChromosomes)
+}
+
+### Test Case
+# testMat <- matrix(sample(c(0,1), 200, replace = T),10,20)
+# testMat - mutate(testMat)
