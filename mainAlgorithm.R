00b60a1084e8b663e16720c3a9d7364d81a072f1
jqling53 <89624295+jqling53@users.noreply.github.com>
2021-12-01 18:05:13 -0800

    Add mainAlgorithm

---
+++ mainAlgorithm.R
@@ -0,0 +1,205 @@
+library(knitr)
+library(dplyr)
+
+
+### Initialize and Generate Genes
+
+# Generate the genes and set the chromosomes:
+create_population <- function(chromosome_length, population_size){
+  n <- chromosome_length * population_size
+  chromosome <- as.vector(sample(0:1, n, replace=TRUE))
+  population <- as.data.frame(matrix(chromosome, nrow = population_size, ncol = chromosome_length))
+  names(population) <- dput(paste0('gen_', seq(1,chromosome_length,1)))
+  return(population)
+}
+
+### Calculate Fitness
+
+# Function to identify the genes that would be active in each model given a chromosome:
+find_genes <- function(chromosome, variables_names){
+  variables <- variables_names[grep(1,as.vector(chromosome))]
+  return(variables)
+}
+
+# Function to construct the formulas given the active genes of a chromosome:
+set_formulas <- function(active_genes, name_y){
+  formulas <- as.formula(paste(name_y, paste(active_genes, sep = "", collapse = " + "), sep = " ~ "))
+  return(formulas)
+}
+
+# Function to compute the fitness (AIC) given a formula and a dataset
+# By default, it will fit a linear regression. Although, it can receive the parameters 
+# for a generalized linear model
+fitness <- function(formula, data, ...){
+  fitness <- AIC(glm(formula = formula, data = data, ...))
+  return(fitness)
+}
+
+# Compute the fitness of an entire generation:
+# The result is sort by the fittest individual to the least fit
+get_fitness <- function(X, name_y, generation){
+  data_names <- names(X)[!names(X) %in% c(name_y)]
+  variables <- apply(generation, 1, find_genes, data_names)
+  formulas <- lapply(variables, set_formulas, name_y)
+  fitness <- lapply(formulas, fitness, X)
+  return(unlist(fitness))
+}
+
+# Be careful, this returns your generation sorted by fitness
+gather_fitness_generation <- function(generation,fitness_scores){
+  gathered <- cbind(generation,fitness_scores) %>%
+    arrange(desc(fitness_scores))
+  return(gathered)
+}
+
+### Select Parents
+
+### INPUT:
+# population: a matrix containing the chromosomes 
+# fitness: a vector of the corresponding fitness of the chromosomes
+# num_partitions: number of partitions in the matrix. Defaults to 
+#                 the number of chromosomes divided by 3
+### OUTPUT:
+# A matrix with containing the best chromosomes from each partition.
+# The number of rows is equal to the number of partitions. Adds the 
+# fitness to the matrix as the last column (at column index: ncol(population)+1)
+tournamentSelection <- function(population, num_partitions=floor(nrow(population)/3)) {
+  columns <- ncol(population) - 1
+  rows <- nrow(population)
+  
+  #Adds fitness as last column to population matrix
+  # combined_matrix <- cbind(population, fitness)
+  combined_matrix <- shuffle_matrix(population)
+  
+  
+  partitions <- matrix_partition(combined_matrix, num_partitions)
+  selectedParents <- t(sapply(partitions, function(x) {
+    x[which.max(x[,columns + 1]),]
+  }))
+  
+  return(selectedParents[,1:(columns)])
+}
+
+
+matrix_partition <- function(matrix, num_partitions) {
+  elements_per_partition <- floor(nrow(matrix)/num_partitions)
+  lapply(1:num_partitions, function(partition_i) { 
+    start_element <- ((partition_i-1)*elements_per_partition)+1
+    end_element <- (partition_i)*elements_per_partition
+    matrix(matrix[start_element:end_element,], nrow=elements_per_partition, ncol=ncol(matrix))
+  })
+}
+
+shuffle_matrix <- function(matrix) {
+  random <- sample(nrow(matrix))
+  matrix[random,]
+}
+
+### Crossover
+
+crossover <- function(parentsA, parentsB) {
+  
+  # Create vector of where to split each chromosome
+  # Random selection
+  splitLocation <- sample(1:(ncol(parentsA) - 1),
+                          size = nrow(parentsA), replace = T)
+  
+  # Combine genes left of the split location from parent A with
+  #   genes right of the split location from parent B
+  breedSingle <- function(A, B, splitIndex) {
+    child <- c(A[1:splitIndex], B[(splitIndex + 1):length(B)])
+    return(child)
+  }
+  
+  # Apply breedSingle to every pair of parents
+  childMatrix <- matrix(rep(0, nrow(parentsA) * ncol(parentsA)),
+                        nrow(parentsA), ncol(parentsA))
+  for (i in 1:nrow(parentsA)) {
+    childMatrix[i,] <- breedSingle(parentsA[i,], parentsB[i,], splitLocation[i])
+  }
+  
+  return(childMatrix)
+}
+
+### Mutate
+
+mutate <- function(chromosomes, mutateProbability = 0.01) {
+  # Inputs: 
+  #   chromosomes: matrix of chromosomes
+  #   mutateProbability: the probability for each gene to mutate. Default
+  #                       is 1%.
+  # Output:
+  #   Matrix of mutated chromosomes
+  
+  # Mutate by subtracting 1 from the element and taking the absolute value
+  #   Changes 0 to 1, changes 1 to 0
+  # Generate mutation matrix from Bernoulli(mutateProbablity) distribution
+  mutatedChromosomes <- abs(chromosomes - 
+                              matrix(rbinom(nrow(chromosomes) * ncol(chromosomes),
+                                            1, mutateProbability), 
+                                     nrow = nrow(chromosomes), ncol = ncol(chromosomes))
+  )
+  return(mutatedChromosomes)
+}
+
+
+
+### Testing
+
+set.seed(123)
+
+mainAlgorithm <- function(chromosome_length = 10,
+                          population_size = sample(chromosome_length:(2*chromosome_length), 1, replace=TRUE),
+                          data,
+                          predictor,
+                          num_partitions = floor(population_size/3),
+                          mutateProbability = 0.01                     
+                          ) {
+  
+  my_generation <- create_population(chromosome_length, population_size)
+  
+  fitness_scores <- get_fitness(data, predictor, my_generation)
+  
+  my_generation_info <- gather_fitness_generation(my_generation, fitness_scores)
+  
+  parentsA <- tournamentSelection(as.matrix(my_generation_info), num_partitions)
+  parentsB <- tournamentSelection(as.matrix(my_generation_info), num_partitions)
+  
+  child <- crossover(parentsA, parentsB)
+  
+  mutated <- mutate(child, mutateProbability)
+  
+  return(mutated)
+}
+
+mainAlgorithm(15, 30, x, "a", 2, 0.05)
+
+# Number of genes per chromosome: number of  covariates in a linear model
+chromosome_length <- 10
+
+# Population size 
+# The paper recommends fo binary encoding of chromosomes to choose P to satisfy $C \leq P \leq 2C$
+population_size <- sample(chromosome_length:(2*chromosome_length), 1, replace=TRUE)
+
+my_generation <- create_population(chromosome_length, population_size)
+head(my_generation)
+
+x <- as.data.frame(matrix(runif(100*(chromosome_length+1),0,1),ncol=(chromosome_length+1),nrow=100))
+names(x) <- letters[1:(chromosome_length+1)]
+head(x)
+
+fitness_scores <- get_fitness(x,"a",my_generation)
+head(fitness_scores)
+
+my_generation_info <- gather_fitness_generation(my_generation, fitness_scores)
+
+
+# fit one by lm
+AIC(lm(a ~ f+h+j+k, data = x )) 
+
+parentsA <- tournamentSelection(as.matrix(my_generation_info))
+parentsB <- tournamentSelection(as.matrix(my_generation_info))
+
+child <- crossover(parentsA, parentsB)
+
+mutate(child)
