f2ebea15c371ee938ec30d4cfcc77dc98c3b98a6
jqling53 <89624295+jqling53@users.noreply.github.com>
2021-12-10 05:31:48 -0800

    added tests for crossover and mutation

--- projectCrossover.R
+++ projectCrossover.R
@@ -2,6 +2,8 @@
 ### Crossover
 ### Jonathan Ling
 
+library(testthat)
+
 ### Default crossover function
 # Arguments: 
 #   parents_A, parents_B: Two parent matrices
@@ -55,9 +57,17 @@ crossover <- function(parentsA, parentsB, num_split = 1) {
   return(child_matrix)
 }
 
-### Test case
-# testMatA <- matrix(rep(0,100),10,10)
-# testMatB <- matrix(rep(1,100),10,10)
-# 
-# testChild <- crossover(testMatA, testMatB, num_split = 3)
-# testChild
+### Testing
+
+test_that("the output is a matrix and is the same size as the parent matrices
+          with values from either parent",
+          {
+            test_mat_A <- matrix(rep(0,100),10,10)
+            test_mat_B <- matrix(rep(1,100),10,10)
+            test_child <- crossover(test_mat_A, test_mat_B, num_split = 3)
+            
+            expect_is(test_child, 'matrix')
+            expect_true(nrow(test_child)==nrow(test_mat_A))
+            expect_true(ncol(test_child)==ncol(test_mat_A))
+            expect_true(test_child == 1 || test_child == 0)
+          })
--- projectMutation.R
+++ projectMutation.R
@@ -2,6 +2,7 @@
 ### Mutation
 ### Jonathan Ling
 
+library(testthat)
 
 mutate <- function(chromosomes, mutateProbability = 0.01) {
   # Inputs: 
@@ -22,6 +23,18 @@ mutate <- function(chromosomes, mutateProbability = 0.01) {
   return(mutatedChromosomes)
 }
 
+
+
 ### Test Case
-# testMat <- matrix(sample(c(0,1), 200, replace = T),10,20)
-# testMat - mutate(testMat)
+test_mat <- matrix(sample(c(0,1), 200, replace = T),10,20)
+mutated_mat <- mutate(test_mat)
+test_mat - mutated_mat
+
+test_that("the output is a matrix of the same size as the input and is binary",
+          {
+            expect_is(mutated_mat, 'matrix')
+            expect_true(nrow(mutated_mat)==nrow(test_mat))
+            expect_true(ncol(mutated_mat)==ncol(test_mat))
+            expect_true(mutated_mat == 1 || mutated_mat == 0)
+            
+          })
