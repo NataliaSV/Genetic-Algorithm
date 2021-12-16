aca8e18f2a66e2462b095d7a2c2a28bea11c2523
jqling53 <89624295+jqling53@users.noreply.github.com>
2021-12-15 18:18:17 -0800

    commented titanic_test.R

--- titanic_test.R
+++ titanic_test.R
@@ -4,6 +4,8 @@ source("select.R")
 
 set.seed(11)
 
+# We are using the titanic_train dataset from the titanic package.
+
 # Convert the gender column to binary
 gender <- rep(-1, nrow(titanic_train))
 for (i in 1:nrow(titanic_train)) {
@@ -18,11 +20,12 @@ for (i in 1:nrow(titanic_train)) {
   }
 }
 
-# Organize the numeric data
 # Add some unrelated columns that the algorithm should choose to leave out
 unrelated_1 <- rnorm(nrow(titanic_train), 0, 100)
 unrelated_2 <- rnorm(nrow(titanic_train), 0, 10) * 20
 
+
+# Organize the numeric data into one matrix
 titanic_data <- cbind(Survived = titanic_train[,2], ID = titanic_train[,1], 
                       Pclass = titanic_train[,3], gender, unrelated_1, 
                       titanic_train[,6:8], unrelated_2, 
@@ -49,6 +52,5 @@ best_titanic
 # We expect columns b (Passenger ID), e, and i (randomly generated numbers) to
 #   be unrelated to the survival variable, so the best fit model should have 0s
 #   in those columns.
-
 
 # AIC(glm(a~c+d+f+g+h+j, data = cleaned_data))
